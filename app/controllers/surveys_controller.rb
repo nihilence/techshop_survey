139121076bb9a7cda6ea1a21a3b863aa90b3fe43
artemis <artemis@MacBook-Pro-3.local>
2017-11-23 19:47:30 -0800

    Adds form, renders to JSON for now

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,5 +1,11 @@
 class SurveysController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def new
     @categories = Category.all
+  end
+
+  def create
+    render json: params
   end
 end
--- db/migrate/20171123002125_create_users.rb
+++ db/migrate/20171123002125_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       t.string :name, null: false
       t.string :email, null: false, unique: true
-      t.integer  :monthly_contribution
+      t.integer :monthly_contribution
       t.integer :months_in_advance, default: 1
       t.integer :funding_donation
       t.text    :funding_details
